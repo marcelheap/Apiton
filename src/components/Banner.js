bd176ecb7ce03fc4558b8b6b0b57f0b354da6757
marcelheap <97971224+marcelheap@users.noreply.github.com>
2022-01-22 17:04:07 +0500

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -59,7 +59,7 @@ const Banner = () => {
               >
                 <input type="text" name="email" placeholder="Email address" />
                 <button type="submit" className="thm-btn banner-one__mc-btn">
-                  <span>Contact</span>
+                  <span>Free Quote</span>
                 </button>
               </form>
               <div className="mc-form__response"></div>
