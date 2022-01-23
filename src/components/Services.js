6937369ebd12ad6424e109e875eb01bcec31586f
marcelheap <97971224+marcelheap@users.noreply.github.com>
2022-01-24 01:24:06 +0500

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -12,62 +12,65 @@ const Services = () => {
           titleText={`MarcelHeap Providing You \n Full-Length Service`}
         />
         <div className="row">
-          <Link to="/design">
           <div className="col-lg-3 col-md-6">
             <div className="service-one__single">
               <div className="service-one__inner">
                 <div className="service-one__circle"></div>
+                <Link to="/design">
                 <div className="service-one__icon">
                   <i className="apton-icon-computer-graphic"></i>
                 </div>
                 <h3>Design</h3>
                 <p>Discover our design services.</p>
+                </Link>
               </div>
             </div>
           </div>
-          </Link>
-          <Link to="/web-development"> 
-            <div className="col-lg-3 col-md-6">
+          
+           <div className="col-lg-3 col-md-6">
             <div className="service-one__single">
               <div className="service-one__inner">
                 <div className="service-one__circle"></div>
+                <Link to="/web-development">
                 <div className="service-one__icon">
                   <i className="apton-icon-development"></i>
                 </div>
                 <h3>Web Development</h3>
                 <p>Discover our web development services.</p>
+                </Link>
               </div>
             </div>
           </div>
-        </Link>
-        <Link to="/digital-marketing">
+        
           <div className="col-lg-3 col-md-6">
             <div className="service-one__single">
               <div className="service-one__inner">
                 <div className="service-one__circle"></div>
+                <Link to="/digital-marketing">
                 <div className="service-one__icon">
                   <i className="apton-icon-development1"></i>
                 </div>
                 <h3>Digital Marketing</h3>
                 <p>Discover our digital marketing services.</p>
+                </Link>
               </div>
             </div>
           </div>
-        </Link>
-        <Link to="/mobile-app-development">
+        
           <div className="col-lg-3 col-md-6">
             <div className="service-one__single">
               <div className="service-one__inner">
                 <div className="service-one__circle"></div>
+                <Link to="/mobile-app-development">
                 <div className="service-one__icon">
                   <i className="apton-icon-responsive"></i>
                 </div>
                 <h3>Mobile App Development</h3>
                 <p>Discover our mobile app development services.</p>
+              </Link>
               </div>
             </div>
           </div>
-        </Link>
         </div>
       </div>
     </section>
