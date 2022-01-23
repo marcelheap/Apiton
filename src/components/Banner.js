2c9fa3eac2adceaaf3c2c2a4ba87934df6d31b92
marcelheap <97971224+marcelheap@users.noreply.github.com>
2022-01-24 02:02:15 +0500

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -48,6 +48,9 @@ const Banner = () => {
                 <button className="thm-btn banner-one__mc-btn">
                   <span>Top-Rated</span>
                 </button>  
+                <h3>
+                Design, 
+                </h3>
               <h3>
                Development &<br /> Marketing <br /> Agency
               </h3>
